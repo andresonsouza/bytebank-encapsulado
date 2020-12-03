91d61b87a8a8be765038afe578213bb28b279387
Andreson Souza <andresonjazz@gmail.com>
2020-12-03 10:40:09 -0300

    crie getters e setters de referência

--- src/TestaGetESet.java
+++ src/TestaGetESet.java
@@ -6,5 +6,22 @@ public class TestaGetESet {
 		// conta.numero = 1337;
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		paulo.setNome("paulo silveira");
+		
+		conta.setTitular(paulo);
+	
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		//agora em dua linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
 	}
 }
